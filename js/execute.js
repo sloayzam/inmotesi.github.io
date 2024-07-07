effd0f2f2f4e0e1080ccc7729bdd13852b8a0c3e
sloayzam <sloayza@inmotesi.com>
2024-07-07 16:51:59 -0500

    Update-070720241651

--- js/execute.js
+++ js/execute.js
@@ -1,11 +1,11 @@
-const open = document.getElementById('open');
-const show = document.getElementById('modal-container-show');
-const close = document.getElementById('close');
+const open = document.getElementById("open");
+const show = document.getElementById("modal-container-show");
+const close = document.getElementById("close");
 
-open.addEventListener('click', () => {
-    show.classList.add('show');  
+open.addEventListener("click", () => {
+    show.classList.add("show");  
 });
 
-close.addEventListener('click', () => {
-    show.classList.remove('show');
+close.addEventListener("click", () => {
+    show.classList.remove("show");
 });
