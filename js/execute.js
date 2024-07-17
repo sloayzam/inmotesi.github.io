caedf5e12a39aa673147341de0e154f13d117de3
sloayzam <sloayza@inmotesi.com>
2024-07-16 23:44:50 -0500

    UPDATE160720242344

--- js/execute.js
+++ js/execute.js
@@ -7,19 +7,3 @@ showwarning.classList.add("show");
 close.addEventListener("click", () => {
     showwarning.classList.remove("show");
 });
-
-
-
-/*PARA CARRUSEL DE IMÁGENES EN CONSTRUCCIÓN Y DISEÑO*/
-$("section.awSlider .carousel").carousel({
-    pause: "hover",
-    interval: 500
-});
-
-var startImage = $("section.awSlider .item.active > img").attr("src");
-$("section.awSlider").append('<img src="' + startImage + '">');
-
-$("section.awSlider .carousel").on("slid.bs.carousel", function () {
-    var bscn = $(this).find(".item.active > img").attr("src");
-    $("section.awSlider > img").attr("src", bscn);
-});
